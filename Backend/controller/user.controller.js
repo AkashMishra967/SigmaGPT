040046bec9b110d57e0b0373ed9a153f01bbb0a0
Akash Mishra <akashmishra967033@gmail.com>
2026-09-14 18:11:37 +0530

    add

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -28,3 +28,38 @@ export const signup = async (req, res) =>{
 }catch(error){
  return res.status(500).json({message:"Server Error"});
 }}
+
+
+
+export const login = async (req,res) =>{
+    try{
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "All Field are required"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid Crenditional"});
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid Creditional"});
+        }
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+        return res.status(200).json({message:"Login Succcessfullly",
+        token : token,
+        user :{
+            id: user._id,
+            email : user.email,
+            username: user.username
+
+        }
+        })
+    }catch(error){
+        return res.status(500).json({message:"Server Error"})
+    }
+}
