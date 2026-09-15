db6d44047e201450217f6ffab6a89e5154af6596
Akash Mishra <akashmishra967033@gmail.com>
2026-09-15 17:58:40 +0530

    add

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -63,3 +63,31 @@ export const login = async (req,res) =>{
         return res.status(500).json({message:"Server Error"})
     }
 }
+
+export const logout = async(req,res) =>{
+    try{
+    res.clearCookie("token",{
+        httpOnly: true,
+        secure: process.env.NODE_ENV ==="production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+    })
+    return res.status(200).json({message:"Logout Successfully"});
+
+}catch(error){
+return res.status(500).json({message:"server error"});
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
